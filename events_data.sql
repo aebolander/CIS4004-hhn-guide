e22aa7d493c7c4f4c628bbbb2043056fb6f73a53
Audrey Bolander <71717909+aebolander@users.noreply.github.com>
2025-04-10 16:37:55 -0400

    Add files via upload

---
+++ events_data.sql
@@ -0,0 +1,16 @@
+INSERT INTO hhn_db.hhn_events
+VALUES (1, "Halloween Fall experience", 2020);
+
+INSERT INTO hhn_db.hhn_events
+VALUES (2, "HHN 30", 2021);
+
+INSERT INTO hhn_db.hhn_events
+VALUES (3, "HHN 31", 2022);
+
+INSERT INTO hhn_db.hhn_events
+VALUES (4, "HHN 32", 2023);
+
+INSERT INTO hhn_db.hhn_events
+VALUES (5, "HHN 33", 2024);
+
+SELECT * FROM hhn_db.hhn_events;
